18e49973740a410fa2947b556adefa5da2217afc
armanphero <017911sksaifurrahman@gmail.com>
2023-11-22 14:11:58 -0800

    done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+function App() {
+
+  return (
+    <>
+      <h1>hello </h1>
+      <MessengerCustomerChat
+        pageId="61551575173649"
+        appId="341427618476980"
+      />
+    </>
+  )
+}
+
+export default App
